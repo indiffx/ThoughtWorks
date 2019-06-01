3d512b30f29aee14161a4770926d588e3386dd1c
indiffx <indiffx@outlook.com>
2019-06-01 10:47:53 +0800

    extended invalid cases

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 module.exports = function main(inputs) {
     const [TAXI_FEE_PER_K, START_FEE, PARK_FEE_PER_MIN] = [0.8, 6, 0.25];
     let [finalPrice, {distance, parkTime}] = [START_FEE, inputs];
-	if (distance < 0 || parkTime < 0) {
-		console.log("Invalid Input: shouldn't be less than zero.")
+	if (isNaN(distance) || isNaN(parkTime) || 
+				distance < 0 || parkTime < 0) {
+		console.log("Invalid Input: should be numbers > 0.")
 		return null;
 	} 
-
 	if (distance > 2) {
 		finalPrice += TAXI_FEE_PER_K * (distance - 2)
 		if (distance > 8) {
