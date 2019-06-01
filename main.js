9e0ea617a85dbd2d57516ddef564a9ebb7fa2875
indiffx <indiffx@outlook.com>
2019-06-01 10:17:48 +0800

    added case for invalid inputs

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 module.exports = function main(inputs) {
-    const [TAXI_FEE_PER_K, START_FEE, PARK_FEE_PER_MIN] = [0.8, 6, 0.25];
+    const [TAXI_FEE_PER_K, START_FEE, PARK_FEE_PER_MIN] = 
+    									[0.8, 6, 0.25];
     let 
     	{distance, parkTime} = inputs,
     	[extraFee, parkFee] = [0, 0];
+	if (distance < 0 || parkTime < 0) {
+		console.log("Invalid Input: shouldn't be less than zero.")
+		return null;
+	}
 	parkFee = PARK_FEE_PER_MIN * parkTime; 
 	if (distance <= 2) {
 		return Math.round(START_FEE + parkFee);
