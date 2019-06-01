df290233bb1e60e4f56ffdb9bd7982466877fcce
indiffx <indiffx@outlook.com>
2019-06-01 10:38:48 +0800

    simplified the structure and delete unnecessary variables

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 module.exports = function main(inputs) {
-    const [TAXI_FEE_PER_K, START_FEE, PARK_FEE_PER_MIN] = 
-    									[0.8, 6, 0.25];
-    let 
-    	{distance, parkTime} = inputs,
-    	[extraFee, parkFee] = [0, 0];
+    const [TAXI_FEE_PER_K, START_FEE, PARK_FEE_PER_MIN] = [0.8, 6, 0.25];
+    let [finalPrice, {distance, parkTime}] = [START_FEE, inputs];
 	if (distance < 0 || parkTime < 0) {
 		console.log("Invalid Input: shouldn't be less than zero.")
 		return null;
-	}
-	parkFee = PARK_FEE_PER_MIN * parkTime; 
-	if (distance <= 2) {
-		return Math.round(START_FEE + parkFee);
-	}
-	if (distance > 8) {
-		extraFee = TAXI_FEE_PER_K * 0.5 * (distance - 8);
-	}
+	} 
 
-	price = Math.round(START_FEE + TAXI_FEE_PER_K * 
-			(distance - 2) + extraFee + parkFee);
-    return price;
+	if (distance > 2) {
+		finalPrice += TAXI_FEE_PER_K * (distance - 2)
+		if (distance > 8) {
+			finalPrice += TAXI_FEE_PER_K * 0.5 * (distance - 8);
+		}
+	}
+    return Math.round(finalPrice + PARK_FEE_PER_MIN * parkTime);
 };
